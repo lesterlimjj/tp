7851f9c9a38e3b93edbc886477c71f75b1314824
ToxOptimism <toxicoptimismben@gmail.com>
2025-03-20 00:57:45 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/commands/AssignListingCommand.java
+++ src/main/java/seedu/address/logic/commands/AssignListingCommand.java
@@ -2,28 +2,15 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
-import seedu.address.model.listing.HouseNumber;
 import seedu.address.model.listing.Listing;
-import seedu.address.model.listing.PostalCode;
-import seedu.address.model.listing.PropertyName;
-import seedu.address.model.listing.UnitNumber;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.PropertyPreference;
-import seedu.address.model.price.PriceRange;
-import seedu.address.model.tag.Tag;
 
 /**
  * Assigns a listing to a person.
