9e59084433ae551b5e4973380b3ff82c15836e28
lesterlimjj <lesterlimjj@gmail.com>
2025-03-20 16:47:12 +0800

    Update AddPreferenceTagCommand messages

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -27,6 +27,7 @@ public class Messages {
     public static final String MESSAGE_TAG_OR_NEW_TAG_REQUIRED = "Provide at least a new tag or existing tag. \n%1$s";
     public static final String MESSAGE_POSTAL_CODE_REQUIRED = "Postal code must be provided.\n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
+    public static final String MESSAGE_INVALID_PERSON_OR_PREFERENCE_DISPLAYED_INDEX = "The person or preference index provided is invalid";
     public static final String MESSAGE_INVALID_LISTING_DISPLAYED_INDEX = "The listing index provided is invalid";
     public static final String MESSAGE_INVALID_OWNER_DISPLAYED_INDEX = "The owner index provided is invalid";
     public static final String MESSAGE_DELETE_PROPERTY_TAG_SUCCESS = "Tag(s) in property %s deleted: %s";
--- src/main/java/seedu/address/logic/parser/AddPreferenceTagCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddPreferenceTagCommandParser.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.Messages.MESSAGE_INVALID_PERSON_OR_PREFERENCE_DISPLAYED_INDEX;
+import static seedu.address.logic.Messages.MESSAGE_TAG_OR_NEW_TAG_REQUIRED;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NEW_TAG;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
@@ -8,7 +10,7 @@ import java.util.List;
 import java.util.Set;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.AddPreferenceCommand;
+import seedu.address.logic.commands.AddListingTagCommand;
 import seedu.address.logic.commands.AddPreferenceTagCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
@@ -27,45 +29,54 @@ public class AddPreferenceTagCommandParser implements Parser<AddPreferenceTagCom
         Index personIndex;
         Index preferenceIndex;
 
-        // Ensure arguments are not empty
-        if (args.trim().isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddPreferenceTagCommand.MESSAGE_USAGE));
-        }
+        checkCommandFormat(argMultimap, args);
+        List<Index> multipleIndices;
 
-        String preamble = argMultimap.getPreamble();
-
-        // Ensure an index is provided
-        if (preamble.isEmpty()) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                    AddPreferenceTagCommand.MESSAGE_USAGE));
+        try {
+            multipleIndices = ParserUtil.parseMultipleIndices(argMultimap.getPreamble());
+        } catch (ParseException pe) {
+            throw new ParseException(String.format(MESSAGE_INVALID_PERSON_OR_PREFERENCE_DISPLAYED_INDEX,
+                    AddPreferenceTagCommand.MESSAGE_USAGE),
+                    pe);
         }
 
         try {
-            List<Index> multipleIndices = ParserUtil.parseMultipleIndices(argMultimap.getPreamble());
             if (multipleIndices.size() != 2) {
                 throw new ParseException("Expected 2 indices");
             }
             personIndex = multipleIndices.get(0);
             preferenceIndex = multipleIndices.get(1);
-
         } catch (ParseException pe) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
                     AddPreferenceTagCommand.MESSAGE_USAGE),
                     pe);
         }
 
+        Set<String> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
+        Set<String> newTagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_NEW_TAG));
+
+        return new AddPreferenceTagCommand(personIndex, preferenceIndex, tagSet, newTagSet);
+    }
+
+    private static void checkCommandFormat(ArgumentMultimap argMultimap, String args) throws ParseException {
+        String preamble = argMultimap.getPreamble();
         boolean hasTags = !(argMultimap.getAllValues(PREFIX_TAG).isEmpty());
         boolean hasNewTags = !(argMultimap.getAllValues(PREFIX_NEW_TAG).isEmpty());
         boolean hasCombinedTags = hasTags || hasNewTags;
 
         if (!hasCombinedTags) {
-            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddPreferenceCommand.MESSAGE_USAGE));
+            throw new ParseException(String.format(MESSAGE_TAG_OR_NEW_TAG_REQUIRED, AddPreferenceTagCommand.MESSAGE_USAGE));
         }
 
-        Set<String> tagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_TAG));
-        Set<String> newTagSet = ParserUtil.parseTags(argMultimap.getAllValues(PREFIX_NEW_TAG));
+        if (args.trim().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    AddPreferenceTagCommand.MESSAGE_USAGE));
+        }
 
-        return new AddPreferenceTagCommand(personIndex, preferenceIndex, tagSet, newTagSet);
+        if (preamble.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                    AddPreferenceTagCommand.MESSAGE_USAGE));
+        }
+
     }
 }
