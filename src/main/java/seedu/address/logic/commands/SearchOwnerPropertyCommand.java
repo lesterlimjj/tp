950c38895eed85ad929a31d7200747ab19a38ba3
lesterlimjj <lesterlimjj@gmail.com>
2025-03-29 12:37:19 +0800

    Update SearchOwnerPropertyCommand exception responses

--- src/main/java/seedu/address/logic/commands/SearchOwnerPropertyCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchOwnerPropertyCommand.java
@@ -53,7 +53,7 @@ public class SearchOwnerPropertyCommand extends Command {
 
         List<Person> lastShownList = model.getFilteredPersonList();
         if (targetIndex.getZeroBased() >= lastShownList.size()) {
-            throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            throw new CommandException(String.format(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX, MESSAGE_USAGE));
         }
 
         Person targetPerson = lastShownList.get(targetIndex.getZeroBased());
