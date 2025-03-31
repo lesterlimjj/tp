97ebf00f6c04af21bc62d3757926813c76bcce9b
Ryuse <Leemuel01234@gmail.com>
2025-03-31 21:06:00 +0800

    Rename matchPerson command to matchPreference

--- src/main/java/seedu/address/logic/commands/MatchPreferenceCommand.java
+++ src/main/java/seedu/address/logic/commands/MatchPreferenceCommand.java
@@ -23,14 +23,14 @@ import seedu.address.model.tag.Tag;
  * Find matches for @{code Person}'s {@code PropertyPreference} based on tags and attributes (i.e. within price range),
  * ranking them based on number of tags and attributes matching.
  */
-public class MatchPersonCommand extends Command {
+public class MatchPreferenceCommand extends Command {
 
-    public static final String COMMAND_WORD = "matchPerson";
+    public static final String COMMAND_WORD = "matchPreference";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Find listings that match a person's property preference identified by index number used "
             + "in the displayed person and preference list.\n"
-            + "Parameters: PERSON_INDEX  (must be a positive integer) PREFERENCE_INDEX (must be a positive integer)\n"
+            + "Parameters: PERSON_INDEX (must be a positive integer) PREFERENCE_INDEX (must be a positive integer)\n"
             + "Example: " + COMMAND_WORD + " 1 2";
 
     public static final String MESSAGE_MATCH_PERSON_SUCCESS = "Matched Listings for %1$s's Preference - "
@@ -40,12 +40,12 @@ public class MatchPersonCommand extends Command {
     private final Index targetPreferenceIndex;
 
     /**
-     * Creates a {@code MatchPersonCommand} to match {@code Listing}s from the specified {@code PropertyPreference}.
+     * Creates a {@code MatchPreferenceCommand} to match {@code Listing}s from the specified {@code PropertyPreference}.
      *
      * @param targetPersonIndex     Index of the person in the filtered person list to delete the preference from.
      * @param targetPreferenceIndex Index of the preference in the person to delete.
      */
-    public MatchPersonCommand(Index targetPersonIndex, Index targetPreferenceIndex) {
+    public MatchPreferenceCommand(Index targetPersonIndex, Index targetPreferenceIndex) {
         this.targetPersonIndex = targetPersonIndex;
         this.targetPreferenceIndex = targetPreferenceIndex;
     }
@@ -101,11 +101,11 @@ public class MatchPersonCommand extends Command {
         }
 
         // instanceof handles nulls
-        if (!(other instanceof MatchPersonCommand)) {
+        if (!(other instanceof MatchPreferenceCommand)) {
             return false;
         }
 
-        MatchPersonCommand otherDeleteCommand = (MatchPersonCommand) other;
+        MatchPreferenceCommand otherDeleteCommand = (MatchPreferenceCommand) other;
         return targetPersonIndex.equals(otherDeleteCommand.targetPersonIndex)
                 && targetPreferenceIndex.equals(otherDeleteCommand.targetPreferenceIndex);
     }
