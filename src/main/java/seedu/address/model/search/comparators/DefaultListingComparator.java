ff2e934c69b9f14580f9075a284df9d53907556b
Ryuse <Leemuel01234@gmail.com>
2025-04-03 01:56:10 +0800

    Fix javadocs

--- src/main/java/seedu/address/model/search/comparators/DefaultListingComparator.java
+++ src/main/java/seedu/address/model/search/comparators/DefaultListingComparator.java
@@ -7,8 +7,7 @@ import java.util.Comparator;
 import seedu.address.model.listing.Listing;
 
 /**
- * Compares two {@code Listing}s based on default criteria (postal code, unit number, and house number).
- * Compares two listings based on:
+ * Compares two {@code Listing}s based on default criteria (postal code, unit number, and house number)
  * 1. Postal code (primary)
  * 2. Unit number (secondary, with nulls first)
  * 3. House number (tertiary)
