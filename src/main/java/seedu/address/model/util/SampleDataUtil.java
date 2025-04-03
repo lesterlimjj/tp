37b95c63d7ca7578443dde6231ac3024e38017d2
Ryuse <Leemuel01234@gmail.com>
2025-04-03 15:39:22 +0800

    Refactor sample data code

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -27,16 +27,13 @@ import seedu.address.model.tag.Tag;
  */
 public class SampleDataUtil {
 
-    public static Set<String> getSampleTagStrings() {
-        Set<String> tags = new HashSet<>();
-        tags.add("hdb");
-        tags.add("condo");
-        tags.add("bungalow");
-        return tags;
+    public static void addSampleTags(AddressBook addressBook) {
+        addressBook.addTag("hdb");
+        addressBook.addTag("condo");
+        addressBook.addTag("bungalow");
     }
 
-    public static List<Person> getSamplePersons(AddressBook addressBook) {
-        List<Person> persons = new ArrayList<>();
+    public static void addSamplePersons(AddressBook addressBook) {
         Tag hdbTag = addressBook.getTagMap().get("HDB");
 
         Person alexYeoh = new Person(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
@@ -49,37 +46,34 @@ public class SampleDataUtil {
 
         alexYeoh.addPropertyPreference(alexYeohPreference);
         hdbTag.addPropertyPreference(alexYeohPreference);
-        persons.add(alexYeoh);
+        addressBook.addPerson(alexYeoh);
 
         Person berniceYu = new Person(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
                 new ArrayList<>(), new ArrayList<>());
-        persons.add(berniceYu);
+        addressBook.addPerson(berniceYu);
 
         Person charlotteOliveiro = new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"),
                 new Email("charlotte@example.com"), new ArrayList<>(), new ArrayList<>());
-        persons.add(charlotteOliveiro);
+        addressBook.addPerson(charlotteOliveiro);
 
         Person davidLi = new Person(new Name("David Li"), new Phone("91031282"),
                 new Email("lidavid@example.com"),
                 new ArrayList<>(), new ArrayList<>());
-        persons.add(davidLi);
+        addressBook.addPerson(davidLi);
 
         Person irfanIbrahim = new Person(new Name("Irfan Ibrahim"), new Phone("92492021"),
                 new Email("irfan@example.com"),
                 new ArrayList<>(), new ArrayList<>());
-        persons.add(irfanIbrahim);
+        addressBook.addPerson(irfanIbrahim);
 
         Person royBalakrishnan = new Person(new Name("Roy Balakrishnan"), new Phone("92624417"),
                 new Email("royb@example.com"),
                 new ArrayList<>(), new ArrayList<>());
-        persons.add(royBalakrishnan);
+        addressBook.addPerson(royBalakrishnan);
 
-        return persons;
     }
 
-    public static List<Listing> getSampleListings(AddressBook addressBook) throws IllegalValueException {
-        List<Listing> listings = new ArrayList<>();
-
+    public static void addSampleListings(AddressBook addressBook) {
         Tag condoTag = addressBook.getTagMap().get("CONDO");
         Tag hdbTag = addressBook.getTagMap().get("HDB");
         Tag bungalowTag = addressBook.getTagMap().get("BUNGALOW");
@@ -95,10 +89,9 @@ public class SampleDataUtil {
 
         List<Person> owners = addressBook.getPersonList();
 
-        Listing sunnyResidences = Listing.of(
+        Listing sunnyResidences = new Listing(
                 new PostalCode("123456"),
                 new UnitNumber("10-15"),
-                null,
                 new PriceRange(new Price("1000000"), new Price("2000000")),
                 new PropertyName("Sunny Residences"),
                 condoTagSet,
@@ -106,11 +99,12 @@ public class SampleDataUtil {
                 true
         );
         condoTag.addListing(sunnyResidences);
-        listings.add(sunnyResidences);
+        addressBook.addListing(sunnyResidences);
+        owners.get(1).addListing(sunnyResidences);
+        addressBook.setPerson(owners.get(1), owners.get(1));
 
-        Listing hdbFlat = Listing.of(
+        Listing hdbFlat = new Listing(
                 new PostalCode("238877"),
-                null,
                 new HouseNumber("42B"),
                 new PriceRange(new Price("100000"), new Price("200000")),
                 null,
@@ -119,12 +113,11 @@ public class SampleDataUtil {
                 true
         );
         hdbTag.addListing(hdbFlat);
-        listings.add(hdbFlat);
+        addressBook.addListing(hdbFlat);
 
-        Listing marinaBaySuites = Listing.of(
+        Listing marinaBaySuites = new Listing(
                 new PostalCode("543210"),
                 new UnitNumber("35-01"),
-                null,
                 new PriceRange(new Price("2000000"), new Price("5000000")),
                 new PropertyName("Marina Bay Suites"),
                 condoTagSet,
@@ -132,11 +125,10 @@ public class SampleDataUtil {
                 false
         );
         condoTag.addListing(marinaBaySuites);
-        listings.add(marinaBaySuites);
+        addressBook.addListing(marinaBaySuites);
 
-        Listing gardenVillas = Listing.of(
+        Listing gardenVillas = new Listing(
                 new PostalCode("159753"),
-                null,
                 new HouseNumber("12"),
                 new PriceRange(new Price("500000"), new Price("600000")),
                 new PropertyName("Garden Villas"),
@@ -145,53 +137,37 @@ public class SampleDataUtil {
                 true
         );
         bungalowTag.addListing(gardenVillas);
-        listings.add(gardenVillas);
+        addressBook.addListing(gardenVillas);
 
-        Listing condoUnit = Listing.of(
+        Listing condoUnit = new Listing(
                 new PostalCode("987654"),
                 new UnitNumber("05-22"),
-                null,
                 new PriceRange(new Price("400000"), new Price("800000")),
                 null,
                 condoTagSet,
                 List.of(owners.get(2), owners.get(3)),
                 true
         );
+
         condoTag.addListing(condoUnit);
-        listings.add(condoUnit);
+        addressBook.addListing(condoUnit);
+        owners.get(2).addListing(condoUnit);
+        addressBook.setPerson(owners.get(2), owners.get(2));
+        owners.get(3).addListing(condoUnit);
+        addressBook.setPerson(owners.get(3), owners.get(3));
+
 
         addressBook.setTag(condoTag, condoTag);
         addressBook.setTag(hdbTag, hdbTag);
         addressBook.setTag(bungalowTag, bungalowTag);
-
-        for (Person owner : owners) {
-            for (Listing listing : listings) {
-                if (listing.getOwners().contains(owner)) {
-                    owner.addListing(listing);
-                }
-            }
-            addressBook.setPerson(owner, owner);
-        }
-
-        return listings;
     }
 
     public static ReadOnlyAddressBook getSampleAddressBook() {
         AddressBook sampleAb = new AddressBook();
 
-        sampleAb.addTags(getSampleTagStrings());
-
-        for (Person samplePerson : getSamplePersons(sampleAb)) {
-            sampleAb.addPerson(samplePerson);
-        }
-
-        try {
-            for (Listing sampleListing : getSampleListings(sampleAb)) {
-                sampleAb.addListing(sampleListing);
-            }
-        } catch (IllegalValueException e) {
-            throw new AssertionError("sample data cannot be invalid", e);
-        }
+        addSampleTags(sampleAb);
+        addSamplePersons(sampleAb);
+        addSampleListings(sampleAb);
 
         return sampleAb;
     }
